6f2b959a13cdb48272812006fe83ed54b50eff3c
DESKTOP-CNVCGR4\phits <maidnoy23@gmail.com>
2020-11-30 18:07:07 +0700

    intern30/11

--- index.php
+++ index.php
@@ -8,18 +8,63 @@
 
     <h1> Todo List </h1>
 
+
 <form action="" method="post">
+<input type="text" name="name" placeholder="New Test" size='50'>
+<input type="submit" value="Add" size="5">
 
-  <label for="name"> <input type="text" name="name" placeholder="New Test" size="50"> <input type="submit" value="Add" size="5"> </label>
+<br>
+<br>
 
-</form>
-      <input type="checkbox" name="checkbox">
+<?php 
 
-      <?php
+if (!empty($_POST['name'])) {
 
-          echo $_POST["name"];
+    ?>
+
+    <input  type='text'  name='name1'  value='<?php echo ($_POST['name']) ?>' >
+   
+    <?php
+
+    if(empty($_POST['name'])){
 
       ?>
-      
+
+        <input name='name' value='<?php echo ($_POST['name']) ?>'>
+
+      <?php
+      }
+    
+    ?>
+    <br>
+
+ <?php
+
+  } 
+?>
+
+<?php 
+
+
+if (!empty($_POST['name1'])) {
+
+ 
+
+    ?>
+
+    <input name='name2'  value='<?php echo ($_POST['name1']) ?>'  >
+    <br>
+
+ <?php
+
+
+  } 
+
+
+?>
+
+</form>
+
+
 </body>
 </html>
